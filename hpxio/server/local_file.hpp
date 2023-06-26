4284964c597d2e775342384bbc70c3379c8895f7
Arnav <arnavnegi14@gmail.com>
2023-06-26 13:18:00 +0530

    trying to restructure the code

--- hpxio/server/local_file.hpp
+++ hpxio/server/local_file.hpp
@@ -4,14 +4,13 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_COMPONENTS_IO_SERVER_LOCAL_FILE_HPP_AUG_27_2014_1200AM)
-#define HPX_COMPONENTS_IO_SERVER_LOCAL_FILE_HPP_AUG_27_2014_1200AM
+#pragma once
 
 #include <hpx/config.hpp>
-#if !defined(HPX_COMPUTE_DEVICE_CODE)
-#include <hpx/include/actions.hpp>
-#include <hpx/include/components.hpp>
 
+#if !defined(HPX_COMPUTE_DEVICE_CODE)
+
+#include <hpx/hpx.hpp>
 #include <hpx/runtime_local/service_executors.hpp>
 
 #include <hpx/execution.hpp>
@@ -30,251 +29,79 @@ typedef _W64 int ssize_t;
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
-namespace hpx::io::server
-        {
-            // local file class
-            // uses C style file APIs
-            class local_file
-                : public hpx::components::locking_hook<
-                      hpx::components::component_base<local_file>>
+namespace hpx::io::server {
+    // local file class
+    // uses C style file APIs
+    class HPX_COMPONENT_EXPORT local_file
+            : public hpx::components::locking_hook<
+                    hpx::components::component_base<local_file>> {
+    public:
+        local_file();
 
-            {
-            public:
-                local_file()
-                {
-                    fp_ = NULL;
-                    file_name_.clear();
-                }
+        ~local_file();
 
-                ~
+        void open(std::string const &name, std::string const &mode);
 
-                    local_file()
-                {
-                    close();
-                }
 
-                void open(std::string const &name, std::string const &mode)
-                {
-                    hpx::parallel::execution::io_pool_executor exec;
-                    hpx::parallel::execution::post(exec, hpx::bind(&local_file::open_work, this, name, mode));
-                }
+        bool is_open() const;
 
-                void open_work(std::string const &name, std::string const &mode)
-                {
-                    if (fp_ != NULL)
-                    {
-                        close();
-                    }
-                    fp_ = fopen(name.c_str(), mode.c_str());
-                    file_name_ = name;
-                }
+        void close();
 
-                bool is_open() const
-                {
-                    return fp_ != NULL;
-                }
+        int remove_file(std::string const &file_name);
 
-                void close()
-                {
-                    hpx::parallel::execution::io_pool_executor exec;
-                    hpx::parallel::execution::post(exec, hpx::bind(&local_file::close_work, this));
-                }
+        std::vector<char> read(size_t count);
 
-                void close_work()
-                {
-                    if (fp_ != NULL)
-                    {
-                        std::fclose(fp_);
-                        fp_ = NULL;
-                    }
-                    file_name_.clear();
-                }
+        std::vector<char> pread(size_t count, off_t offset);
 
-                int remove_file(std::string const &file_name)
-                {
-                    int result;
-                    {
-                        hpx::parallel::execution::io_pool_executor exec;
-                        hpx::parallel::execution::post(exec, hpx::bind(&local_file::remove_file_work, this, file_name, std::ref(result)));
-                    }
-                    return result;
-                }
+        ssize_t write(std::vector<char> const &buf);
 
-                void remove_file_work(std::string const &file_name, int &result)
-                {
-                    result = std::remove(file_name.c_str());
-                }
+        ssize_t pwrite(std::vector<char> const &buf,
+                       off_t offset);
 
-                std::vector<char> read(size_t const count)
-                {
-                    std::vector<char> result;
-                    {
-                        hpx::parallel::execution::io_pool_executor exec;
-                        hpx::parallel::execution::async_execute(exec, hpx::bind(&local_file::read_work, this, count, std::ref(result))).get();
-                    }
-                    return result;
-                }
+        int lseek(off_t offset, int whence);
 
-                void read_work(size_t const count, std::vector<char> &result)
-                {
-                    if (fp_ == NULL || count <= 0)
-                    {
-                        return;
-                    }
+        ///////////////////////////////////////////////////////////////////////
+        /// Each of the exposed functions needs to be encapsulated into a action
+        /// type, allowing to generate all require boilerplate code for threads,
+        /// serialization, etc.
+        HPX_DEFINE_COMPONENT_ACTION(local_file, open);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, is_open);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, close);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, remove_file);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, read);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, pread);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, write);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, pwrite);
+        HPX_DEFINE_COMPONENT_ACTION(local_file, lseek);
 
-                    std::unique_ptr<char> sp(new char[count]);
-                    ssize_t len = fread(sp.get(), 1, count, fp_);
+    private:
+        void open_work(std::string const &name, std::string const &mode);
 
-                    if (len == 0)
-                    {
-                        return;
-                    }
+        void close_work();
 
-                    result.assign(sp.get(), sp.get() + len);
-                }
+        void remove_file_work(std::string const &file_name, int &result);
 
-                std::vector<char> pread(size_t const count, off_t const offset)
-                {
-                    std::vector<char> result;
-                    {
-                        hpx::parallel::execution::io_pool_executor exec;
-                        hpx::parallel::execution::async_execute(exec, hpx::bind(&local_file::pread_work, this, count, offset, std::ref(result))).get();
-                    }
-                    return result;
-                }
+        void read_work(size_t count, std::vector<char> &result);
 
-                void pread_work(size_t const count, off_t const offset,
-                                std::vector<char> &result)
-                {
-                    if (fp_ == NULL || count <= 0 || offset < 0)
-                    {
-                        return;
-                    }
+        void pread_work(size_t count, off_t offset,
+                        std::vector<char> &result);
 
-                    fpos_t pos;
-                    if (fgetpos(fp_, &pos) != 0)
-                    {
-                        return;
-                    }
+        void write_work(std::vector<char> const &buf,
+                        ssize_t &result);
 
-                    if (fseek(fp_, offset, SEEK_SET) != 0)
-                    {
-                        fsetpos(fp_, &pos);
-                        return;
-                    }
+        void pwrite_work(std::vector<char> const &buf,
+                         off_t offset, ssize_t &result);
 
-                    read_work(count, result);
-                    fsetpos(fp_, &pos);
-                }
+        void lseek_work(off_t offset, int whence, int &result);
 
-                ssize_t write(std::vector<char> const &buf)
-                {
-                    ssize_t result = 0;
-                    {
-                        hpx::parallel::execution::io_pool_executor exec;
-                        hpx::parallel::execution::async_execute(exec, hpx::bind(&local_file::write_work, this, buf, std::ref(result))).get();
-                    }
-                    return result;
-                }
+    private:
+        typedef hpx::components::managed_component_base<local_file> base_type;
 
-                void write_work(std::vector<char> const &buf,
-                                ssize_t &result)
-                {
-                    if (fp_ == NULL || buf.
+        std::FILE *fp_;
+        std::string file_name_;
+    };
 
-                                       empty()
-
-                    )
-                    {
-                        return;
-                    }
-                    result = fwrite(buf.data(), 1, buf.size(), fp_);
-                }
-
-                ssize_t pwrite(std::vector<char> const &buf,
-                               off_t const offset)
-                {
-                    ssize_t result = 0;
-                    {
-                        hpx::parallel::execution::io_pool_executor exec;
-                        hpx::parallel::execution::async_execute(exec, hpx::bind(&local_file::pwrite_work, this, buf, offset, std::ref(result))).get();
-                    }
-                    return result;
-                }
-
-                void pwrite_work(std::vector<char> const &buf,
-                                 off_t const offset, ssize_t &result)
-                {
-                    if (fp_ == NULL || buf.
-
-                                       empty()
-
-                        || offset < 0)
-                    {
-                        return;
-                    }
-
-                    fpos_t pos;
-                    if (
-                        fgetpos(fp_, &pos) != 0)
-                    {
-                        return;
-                    }
-
-                    if (
-                        fseek(fp_, offset, SEEK_SET) != 0)
-                    {
-                        fsetpos(fp_, &pos);
-                        return;
-                    }
-
-                    write_work(buf, result);
-                    fsetpos(fp_, &pos);
-                }
-
-                int lseek(off_t const offset, int const whence)
-                {
-                    int result;
-                    {
-                        hpx::parallel::execution::io_pool_executor exec;
-                        hpx::parallel::execution::async_execute(exec, hpx::bind(&local_file::lseek_work, this, offset, whence, std::ref(result))).get();
-                    }
-                    return result;
-                }
-
-                void lseek_work(off_t const offset, int const whence, int &result)
-                {
-                    if (fp_ == NULL)
-                    {
-                        result = -1;
-                        return;
-                    }
-
-                    result = fseek(fp_, offset, whence);
-                }
-
-                ///////////////////////////////////////////////////////////////////////
-                /// Each of the exposed functions needs to be encapsulated into a action
-                /// type, allowing to generate all require boilerplate code for threads,
-                /// serialization, etc.
-                HPX_DEFINE_COMPONENT_ACTION(local_file, open);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, is_open);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, close);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, remove_file);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, read);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, pread);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, write);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, pwrite);
-                HPX_DEFINE_COMPONENT_ACTION(local_file, lseek);
-
-            private:
-                typedef hpx::components::managed_component_base<local_file> base_type;
-
-                std::FILE *fp_;
-                std::string file_name_;
-            };
-
-        } // hpx::io::server
+} // hpx::io::server
 
 ///////////////////////////////////////////////////////////////////////////////
 // Declaration of serialization support for the local_file actions
@@ -297,5 +124,13 @@ HPX_REGISTER_ACTION_DECLARATION(hpx::io::server::local_file::pwrite_action,
 HPX_REGISTER_ACTION_DECLARATION(hpx::io::server::local_file::lseek_action,
                                 local_file_lseek_action)
 
-#endif
+/// What do these do? Might be needed for linking
+HPX_REGISTER_ACTION_DECLARATION(hpx::lcos::base_lco_with_value<
+        std::vector<std::vector<double>>>::set_value_action,
+        set_value_action_vector_vector_double)
+
+HPX_REGISTER_ACTION_DECLARATION(hpx::lcos::base_lco_with_value<
+        std::vector<double>>::set_value_action,
+        set_value_action_vector_double)
+
 #endif
